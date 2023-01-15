e2058764defc51c31929f77606e7b470ac688dde
HenriqueSilva367 <jhenrique367@gmail.com>
2023-01-15 03:10:51 -0300

    token adicionado

--- backend/src/Controllers/user/signIn.ts
+++ backend/src/Controllers/user/signIn.ts
@@ -7,11 +7,16 @@ export async function signIn(req: Request, res: Response) {
       password,
     } = req.body;
 
+    const token = 'pk8fEetCw9~p4!$@y@i8mKao$fXtKz6z2^z~DS';
+
     if(username !== 'desafiosharenergy' || password !== 'sh@r3n3rgy'){
       return res.status(400).json('Usuário ou Senha não conferem');
     }
 
-    return res.status(200).json('Login Sucesso!');
+    return res.status(201).json({
+      user: username,
+      token,
+    });
   } catch (error) {
     res.sendStatus(500);
   }
